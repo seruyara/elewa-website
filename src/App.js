d831fea264b06da31ae345a084a106a57f97ee2e
Sheila Seruya <Sheilaseruya@gmail.com>
2023-11-03 16:00:06 +0300

    📝 feat:SocialImpactSection1 documentation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, /*Route,*/ Routes } from 'react-router-dom';
 import './App.css';
-import SocialImpact from './components/SocialImpacr';
+import SocialImpact from './components/SocialImpact';
 
 
 function App() {
---
+++ src/components/SocialImpactSection1.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function SocialImpactSection1() {
+  return (
+    <section className="impact-section">
+      <div className="sections">
+        <h1 className="value">733+</h1>
+        <p className="title">People tarined</p>
+      </div>
+      <div className="sections">
+        <h1 className="value">733k+</h1>
+        <p className="title">careers launched</p>
+      </div>
+      <div className="sections">
+        <h1 className="value">200+</h1>
+        <p className="title">Projects finished</p>
+      </div>
+      <div className="sections">
+        <h1 className="value">56</h1>
+        <p className="title">Investors</p>
+      </div>
+      <div className="text-content">
+        <h1 className="title">(Social) business for the win</h1>
+        <p className="paragraph">
+          With a young dynamic population that is eager to contribute to the
+          global economy, East Africa has a lot to offer to the world. Now is
+          the time to nurture sustainable, inclusive and scalable business. The
+          rest will take care of itself.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default SocialImpactSection1;
