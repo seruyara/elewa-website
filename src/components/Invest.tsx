28e73598a44063312f0ffff0a7ab69d4d495ea65
Sheila Seruya <Sheilaseruya@gmail.com>
2023-11-04 23:54:21 +0300

    📝feat:added the impactSection3 tags, classnames and text

--- src/components/Invest.tsx
+++ src/components/Invest.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import Footer from './Footer';
 import InvestHomepage from './InvestHomepage';
+import InvestSection3 from './InvestSection3';
 
 
 function Invest(){
     return(
         <div className='invest'>
          <InvestHomepage/>
+         <InvestSection3/>
          <Footer/>
         </div>
     );
---
+++ src/components/InvestSection3.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+
+function InvestSection3() {
+  return (
+    <section className="impact-section beyond-section">
+      <div className="image-left">
+        <div className="image-content">
+          <img
+            className="img"
+            alt="group-pic"
+            src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690303/elewa-group-website/Images/IMG_6523_vabpyo.png"
+          />
+        </div>
+        <div className="text-content">
+          <h1 className="title2 black">
+            Elewa NV, a multi-stakeholder cooperation
+          </h1>
+          <p className="paragraph black">
+            At the heart of Elewa lies the structure Elewa NV. Based in
+            Brussels, Belgium, Elewa NV is a private holding company which
+            controls the assets of all Elewa activities.
+          </p>
+          <p className="paragraph black">
+            Elewa NV is owned and controlled by Elewa's founder, a small
+            community of investors which share in the vision of Elewa and Elewa
+            employees based throughout the activities of the group.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default InvestSection3;
