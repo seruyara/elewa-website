a881942ed83eb86c84066ca292019357c35134c5
Sheila Seruya <Sheilaseruya@gmail.com>
2023-11-04 09:00:21 +0300

    📝feat:adding the image,title and paragraph for the SocialImpactSection4

--- src/components/SocialImpactSection4.tsx
+++ src/components/SocialImpactSection4.tsx
@@ -1,11 +1,30 @@
-import * as React from 'react'
+import * as React from "react";
 
-function SocialImpactSection4(){
-    return(
-        <section>
-
-        </section>
-    )
+function SocialImpactSection4() {
+  return (
+    <section className="impact-section">
+      <div className="image-right">
+        <h1 className="title2">Open knowledge</h1>
+        <p className="paragraph">
+          What we learn, we share. Through community events, open knowledge
+          repositories, regular teaching moments (everyone a teacher) and
+          academic partnerships.
+        </p>
+        <p className="paragraph">
+          We build for today, with a lens on tomorrow.
+        </p>
+        <p className="paragraph">
+          We build for today, with a lens on tomorrow.
+        </p>
+      </div>
+      <div>
+        <img
+          _ngcontent-ybq-c150=""
+          src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690302/elewa-group-website/Images/unnamed_1_m3dvll.png"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default SocialImpactSection4;
