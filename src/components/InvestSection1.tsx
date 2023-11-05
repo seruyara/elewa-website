4e6b223a3962ba3194f315f541bfefcf08b2c834
Sheila Seruya <Sheilaseruya@gmail.com>
2023-11-05 17:07:15 +0300

    📝 feat:added investSection1 text and tags

---
+++ src/components/InvestSection1.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+function InvestSection1() {
+  return (
+    <section>
+      <div>
+        <div>
+          <h5 className="title2">How we see it differently</h5>
+          <p className="paragraph">
+            The global South is rapidly catching up with the "developed" world.
+            It's time for you to get to know the rapid development, innovation
+            and drive going on in this part of the world (East-Africa, South
+            Asia and beyond).
+          </p>
+        </div>
+        <div>
+          <img
+            src="https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,w_1280/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png"
+            alt="elewaimage"
+            className="img img-big"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default InvestSection1;
