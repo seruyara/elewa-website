e287764da4beb25ed0d6adc3e70c2be9134db09e
Sheila Seruya <Sheilaseruya@gmail.com>
2023-11-05 12:20:41 +0300

    📝feat:added the impactSection6 tags, classnames and text

--- src/components/Invest.tsx
+++ src/components/Invest.tsx
@@ -4,6 +4,7 @@ import InvestHomepage from './InvestHomepage';
 import InvestSection3 from './InvestSection3';
 import InvestSection4 from './InvestSection4';
 import InvestSection5 from './InvestSection5';
+import InvestSection6 from './InvestSection6';
 
 
 function Invest(){
@@ -13,6 +14,7 @@ function Invest(){
          <InvestSection3/>
          <InvestSection4/>
          <InvestSection5/>
+         <InvestSection6/>
          <Footer/>
         </div>
     );
