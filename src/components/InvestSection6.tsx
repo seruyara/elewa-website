b0a5e85099f48a043b464917d1a8e674a73fe218
Sheila Seruya <Sheilaseruya@gmail.com>
2023-11-05 10:25:55 +0300

    📝feat:added the impactSection6 tags, classnames and text

---
+++ src/components/InvestSection6.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+function InvestSection6(){
+    return(
+        <section>
+            <span>  Become part of our mission. </span>
+            <span> And start 
+                <a href="link">investing</a>
+            </span>
+        </section>
+    );
+}
+
+export default InvestSection6
