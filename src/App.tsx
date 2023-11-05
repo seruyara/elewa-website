bf60f7463521105bef5e2003c045f35c7e39dac9
Sheila Seruya <Sheilaseruya@gmail.com>
2023-11-05 12:46:25 +0300

    📝feat:added the aboutus page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import AboutUs from './components/AboutUs';
 import Invest from './components/Invest';
 import SocialImpact from './components/SocialImpact';
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
          <Route path='social-impact' element={<SocialImpact/>}/>
          <Route path='invest' element={<Invest/>}/>
+         <Route path='about' element={<AboutUs/>}/>
         </Routes>
       </div>
     </Router>
---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+function AboutUs(){
+    return(
+        <div className='about'>
+           
+        </div>
+    );
+}
+
+export default AboutUs
